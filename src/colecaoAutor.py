0eac238584544d389df3f3d1c837fe6d811b86bc
IgorNascAlves <igor.nascimento@caelum.com>
2020-03-10 11:57:51 -0300

    metodo busca na Classe ColecaoDeAutores e 3 novos testes

--- src/colecaoAutor.py
+++ src/colecaoAutor.py
@@ -9,6 +9,12 @@ class ColecaoDeAutores:
             raise Exception("Autor com mesmo email")    
         self.__lista.append(autor)
 
+    def busca_por_email(self, email):
+        for item in self.lista_de_autores:
+            if item.email == email:
+                return item
+        raise Exception("Autor não encontrado")
+
     @property
     def lista_de_autores(self):
         return self.__lista[:]
