191c84876e94dfda612706fef35cac03ed0bef98
Igor do Nascimento Alves <igor_flipe@hotmail.com>
2020-04-03 14:30:29 -0300

    fim da implementacao da classe livro e do seus testes

--- src/livro.py
+++ src/livro.py
@@ -1,4 +1,7 @@
+from datetime import datetime, date
+import re
 class Livro:
+    
     def __init__(self, titulo, resumo, sumario,
                 preco, num_paginas, isbn, data,
                 categoria):
@@ -19,19 +22,48 @@ class Livro:
     def _set_resumo(self, resumo):
         if resumo in [None, '', ' ']:
             raise Exception("Resumo não pode ser Nulo")
+        if len(resumo) > 500:
+            raise Exception('Resumo não pode ter mais de 500 caracteres')
         self.__resumo = resumo
 
     def _set_preco(self, preco):
+        if preco in [None]:
+            raise Exception("preco não pode ser Nulo")
+        if preco < 20:
+            raise Exception("Preço não pode ser inferior a 20 reias")
         self.__preco = preco
 
     def _set_num_paginas(self, num_paginas):
+        if num_paginas in [None]:
+            raise Exception("Numera de pagina não pode ser Nulo")
+        if num_paginas < 100:
+            raise Exception("Numero de pagina não pode ser inferior a 100")
         self.__num_paginas = num_paginas
 
     def _set_isbn(self, isbn):
+        if isbn in [None]:
+            raise Exception("isbn não pode ser Nulo")
         self.__isbn = isbn
 
     def _set_data(self, data):
+        if not self._data_valida(data):
+            raise Exception("Data deve ser futura {}".format(data))
         self.__data = data
 
     def _set_categoria(self, categoria):
+        if categoria in [None]:
+            raise Exception("categoria não pode ser Nulo")
         self.__categoria = categoria
+
+    def _cria_objeto_data(self, data):
+        padrao = '([0-9]{2})/([0-9]{2})/([0-9]{4})'
+        data = re.search(padrao, data)
+        dia, mes, ano = int(data.group(1)), int(data.group(2)), int(data.group(3))
+        data = date(day = dia, month = mes, year = ano)
+        
+        return data
+    
+    def _data_valida(self, data):
+        data = self._cria_objeto_data(data)
+        hoje = datetime.today().date()
+        return data >= hoje
--- test/test_livro.py
+++ test/test_livro.py
@@ -1,4 +1,5 @@
 import pytest
+from datetime import datetime, timedelta
 from src.livro import Livro
 
 
@@ -6,58 +7,135 @@ class TestLivro:
 
     @pytest.fixture
     def colecao_de_autores(self):
-        pass
-        #return Livro()
-
-    def test_quando_CriarLivro_NaoDispara_Exception(self):
-        
-        titulo = "Machine Learning em COBOL" #Obrigatorio
-        resumo = 'M'*500 #Até 500 #Obrigatorio
+        titulo = "Machine Learning em COBOL"
+        resumo = 'M'*500 
         sumario = '<br> **** \n <html>'*50
-        preco = 50.20 #a partir de 20 #Obrigatorio
-        num_paginas = 150 #a partir de 100 #Obrigatorio
-        isbn = '978-3-16-148410-0' #formato livre #obrigatorio
-        data = '02/04/2020' #futuro
-        categoria = 'Machine Learning' #nao nula
-        
-        livro = Livro(titulo, resumo, sumario,
-                    preco, num_paginas, isbn, data, categoria)
+        preco = 50.20 
+        num_paginas = 150 
+        isbn = '978-3-16-148410-0'
+        data = datetime.today().date().strftime('%d/%m/%Y')
+        categoria = 'Machine Learning'
 
-    def test_quando_CriaLivroComTituloNulo_dispara_Exception(self):
+        return {'titulo' : titulo, 'resumo' : resumo, 'sumario' : sumario,
+         'preco' : preco, 'num_paginas' : num_paginas, 'isbn' : isbn, 'data' : data,
+          'categoria' : categoria}
+
+    def test_quando_CriarLivro_NaoDispara_Exception(self, colecao_de_autores):
+        
+        livro = Livro(colecao_de_autores['titulo'], colecao_de_autores['resumo'],
+        colecao_de_autores['sumario'], colecao_de_autores['preco'], colecao_de_autores['num_paginas'],
+        colecao_de_autores['isbn'], colecao_de_autores['data'], colecao_de_autores['categoria'])
+
+    def test_quando_CriaLivroComTituloNulo_dispara_Exception(self, colecao_de_autores):
 
         titulo = None #Obrigatorio
-        resumo = 'M'*500 #Até 500 #Obrigatorio
-        sumario = '<br> **** \n <html>'*50
-        preco = 50.20 #a partir de 20 #Obrigatorio
-        num_paginas = 150 #a partir de 100 #Obrigatorio
-        isbn = '978-3-16-148410-0' #formato livre #obrigatorio
-        data = '02/04/2020' #futuro
-        categoria = 'Machine Learning' #nao nula
         
         with pytest.raises(Exception):                
-            livro = Livro(titulo, resumo, sumario,
-                        preco, num_paginas, isbn, data, categoria)
+            livro = Livro(titulo, colecao_de_autores['resumo'], colecao_de_autores['sumario'],
+             colecao_de_autores['preco'], colecao_de_autores['num_paginas'],
+                        colecao_de_autores['isbn'], colecao_de_autores['data'],
+                         colecao_de_autores['categoria'])
     
-    def test_quando_CriaLivroComResumoNulo_dispara_Exception(self):
+    def test_quando_CriaLivroComResumoNulo_dispara_Exception(self, colecao_de_autores):
 
-        titulo = "Machine Learning em COBOL" #Obrigatorio
-        resumo = None #Até 500 #Obrigatorio
-        sumario = '<br> **** \n <html>'*50
-        preco = 50.20 #a partir de 20 #Obrigatorio
-        num_paginas = 150 #a partir de 100 #Obrigatorio
-        isbn = '978-3-16-148410-0' #formato livre #obrigatorio
-        data = '02/04/2020' #futuro
-        categoria = 'Machine Learning' #nao nula
+        resumo = None #Obrigatorio
         
         with pytest.raises(Exception):                
-            livro = Livro(titulo, resumo, sumario,
-                        preco, num_paginas, isbn, data, categoria)
+            livro = Livro(colecao_de_autores['titulo'], resumo,
+            colecao_de_autores['sumario'], colecao_de_autores['preco'], colecao_de_autores['num_paginas'],
+            colecao_de_autores['isbn'], colecao_de_autores['data'], colecao_de_autores['categoria'])
+    
+    def test_quando_CriaLivroComResumoComMaisDe500Chars_dispara_Exception(self, colecao_de_autores):
+
+        resumo = 'M' * 501 #Até 500
         
+        with pytest.raises(Exception):                
+            livro = Livro(colecao_de_autores['titulo'], resumo,
+            colecao_de_autores['sumario'], colecao_de_autores['preco'], colecao_de_autores['num_paginas'],
+            colecao_de_autores['isbn'], colecao_de_autores['data'], colecao_de_autores['categoria'])
+        
+    def test_quando_CriaLivroComPrecoNulo_dispara_Exception(self, colecao_de_autores):
 
+        preco = None #Obrigatorio
+        
+        with pytest.raises(Exception):                
+            livro = Livro(colecao_de_autores['titulo'], colecao_de_autores['resumo'],
+                        colecao_de_autores['sumario'], preco, colecao_de_autores['num_paginas'],
+                        colecao_de_autores['isbn'], colecao_de_autores['data'],
+                         colecao_de_autores['categoria'])
 
-    #def test_quando_CriarMultiplosAutoresDeEmailsDiferentes_NaoDisparaException(self):
+    def test_quando_CriaLivroComPrecoMenorQue20_dispara_Exception(self, colecao_de_autores):
 
-        #assert 0 == 2
+        preco = 19.99 #a partir de 20
+        
+        with pytest.raises(Exception):                
+            livro = Livro(colecao_de_autores['titulo'], colecao_de_autores['resumo'],
+                        colecao_de_autores['sumario'], preco, colecao_de_autores['num_paginas'],
+                        colecao_de_autores['isbn'], colecao_de_autores['data'],
+                         colecao_de_autores['categoria'])
 
-        #with pytest.raises(Exception):
-            #pass
+    def test_quando_CriaLivroComNumDePaginasNulo_dispara_Exception(self, colecao_de_autores):
+
+        num_paginas = None #Obrigatorio
+        
+        with pytest.raises(Exception):                
+            livro = Livro(colecao_de_autores['titulo'], colecao_de_autores['resumo'],
+                        colecao_de_autores['sumario'], colecao_de_autores['preco'],
+                        num_paginas, colecao_de_autores['isbn'], 
+                        colecao_de_autores['data'], colecao_de_autores['categoria'])
+
+    def test_quando_CriaLivroComNumDePaginasMenorQue100_dispara_Exception(self, colecao_de_autores):
+
+        num_paginas = 99 #a partir de 100
+        
+        with pytest.raises(Exception):                
+            livro = Livro(colecao_de_autores['titulo'], colecao_de_autores['resumo'],
+                        colecao_de_autores['sumario'], colecao_de_autores['preco'],
+                        num_paginas, colecao_de_autores['isbn'], 
+                        colecao_de_autores['data'], colecao_de_autores['categoria'])
+
+    def test_quando_CriaLivroComIsbnNulo_dispara_Exception(self, colecao_de_autores):
+        
+        isbn = None #obrigatorio
+
+        with pytest.raises(Exception):
+            livro = Livro(colecao_de_autores['titulo'], colecao_de_autores['resumo'],
+             colecao_de_autores['sumario'], colecao_de_autores['preco'],  colecao_de_autores['num_paginas'],
+             isbn,  colecao_de_autores['data'], colecao_de_autores['categoria'])
+
+    def test_quando_CriaLivroComIsbnFormatado_NaoDispara_Exception(self, colecao_de_autores):
+        
+        isbn = '978-3-16-148410-0' #formato livre
+
+        livro = Livro(colecao_de_autores['titulo'], colecao_de_autores['resumo'],
+        colecao_de_autores['sumario'], colecao_de_autores['preco'],  colecao_de_autores['num_paginas'],
+        isbn,  colecao_de_autores['data'], colecao_de_autores['categoria'])
+
+    def test_quando_CriaLivroComIsbnNaoFormatado_NaoDispara_Exception(self, colecao_de_autores):
+    
+        isbn = '9783161484100' #formato livre
+
+        livro = Livro(colecao_de_autores['titulo'], colecao_de_autores['resumo'],
+            colecao_de_autores['sumario'], colecao_de_autores['preco'],  colecao_de_autores['num_paginas'],
+            isbn,  colecao_de_autores['data'], colecao_de_autores['categoria'])
+    
+    def test_quando_CriaLivroComDataMenorQueDataAtual_dispara_Exception(self, colecao_de_autores):
+        hoje = datetime.today().date()
+        ontem = hoje - timedelta(days = 1)
+        data =  ontem.strftime('%d/%m/%Y')#futuro
+
+        with pytest.raises(Exception):
+            livro = Livro(colecao_de_autores['titulo'], colecao_de_autores['resumo'],
+                colecao_de_autores['sumario'], colecao_de_autores['preco'],  colecao_de_autores['num_paginas'],
+                colecao_de_autores['isbn'], data, colecao_de_autores['categoria'])
+
+    def test_quando_CriaLivroComCategoriaNulo_dispara_Exception(self, colecao_de_autores):
+        
+        categoria = None #nao nula
+
+        with pytest.raises(Exception):
+            livro = Livro(colecao_de_autores['titulo'], colecao_de_autores['resumo'],
+             colecao_de_autores['sumario'], colecao_de_autores['preco'],  colecao_de_autores['num_paginas'],
+             colecao_de_autores['isbn'],  colecao_de_autores['data'], categoria)
+    
+    #assert 0 == 2
